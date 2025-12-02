e4be7ae1124f0834b7e2cc735aeb370eab53b137
pc_self <941388287@qq.com>
2025-12-02 16:23:09 +0800

    Net update frequency

--- Source/Blaster/Private/Character/BlasterCharacter.cpp
+++ Source/Blaster/Private/Character/BlasterCharacter.cpp
@@ -47,6 +47,8 @@ ABlasterCharacter::ABlasterCharacter()
 	GetMesh()->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
 
 	TurningInPlace = ETurningInPlace::ETIP_NotTurning;
+	NetUpdateFrequency = 66.f;
+	MinNetUpdateFrequency = 33.f;
 }
 
 // 用于指定哪些变量需要在网络游戏中同步到所有客户端。
