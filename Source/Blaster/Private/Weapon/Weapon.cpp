dc86dd56d51025792b49ff3df2cfb1de501335be
pc_self <941388287@qq.com>
2026-01-07 04:56:12 +0800

    Show the widget locally

--- Source/Blaster/Private/Weapon/Weapon.cpp
+++ Source/Blaster/Private/Weapon/Weapon.cpp
@@ -56,13 +56,10 @@ void AWeapon::BeginPlay()
 	Super::BeginPlay();
 	PickupWidget->SetVisibility(false);
 
-	if (HasAuthority())
-	{
-		AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
-		AreaSphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
-		AreaSphere->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::OnSphereOverlap);
-		AreaSphere->OnComponentEndOverlap.AddDynamic(this, &AWeapon::OnSphereEndOverlap);
-	}
+	AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	AreaSphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	AreaSphere->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::OnSphereOverlap);
+	AreaSphere->OnComponentEndOverlap.AddDynamic(this, &AWeapon::OnSphereEndOverlap);
 }
 
 void AWeapon::OnWeaponStateSet()
